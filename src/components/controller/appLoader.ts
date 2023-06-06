85c0d5a2ce43c899c8b140b402f0b0b6ec114b77
EvgenKlo <klokovevgen@gmail.com>
2023-06-06 23:47:00 +0500

    refactor: changed file extensions to .ts

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'e1e32f2036b8400f9b4f8a31e07b3e87', // получите свой ключ https://newsapi.org/
         });
     }
 }
